b9357da8d6156252ddf170004718e38f5fed9d9a
Nicolas Berger <contact@nberger.ch>
2020-08-28 16:41:27 +0200

    Update phonofont.py

--- src/phonofont.py
+++ src/phonofont.py
@@ -1,7 +1,9 @@
 import fontforge
 from fontTools.ttLib import TTFont
 from fontTools.feaLib.builder import addOpenTypeFeatures
+from pathlib import Path
 
 ft_font = TTFont('fonts/Andika/AndikaNewBasic-R.ttf')
 addOpenTypeFeatures(ft_font, 'src/rules.fea', tables=['GSUB'])
+Path('artifact').mkdir(parents=True, exist_ok=True)
 ft_font.save('artifact/output.ttf')

7b59c308f4ecf0bdd3d9beac0c4cc17194ac37e2
Nicolas Berger <contact@nberger.ch>
2020-08-28 14:13:03 +0200

    initial structure

---
+++ src/phonofont.py
@@ -0,0 +1,7 @@
+import fontforge
+from fontTools.ttLib import TTFont
+from fontTools.feaLib.builder import addOpenTypeFeatures
+
+ft_font = TTFont('../fonts/Andika/AndikaNewBasic-R.ttf')
+addOpenTypeFeatures(ft_font, 'rules.fea', tables=['GSUB'])
+ft_font.save('../artifact/output.ttf')

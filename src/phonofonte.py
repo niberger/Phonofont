533f14de80b9ca56db33ff68cfa7a2176a7d7f99
Nicolas Berger <contact@nberger.ch>
2020-08-31 12:18:11 +0200

    remove fontforge

--- src/phonofonte.py
+++ src/phonofonte.py
@@ -4,13 +4,10 @@
 
 import sys
 import argparse
-import re
-import fontforge
+from fontTools.ttLib.tables._g_l_y_f import Glyph
 from fontTools.ttLib import TTFont
 from fontTools.feaLib.builder import addOpenTypeFeatures
 from pathlib import Path
-
-FONT_NAME_RE = re.compile(r'^([^-]*)(?:(-.*))?$')
 
 def get_argparser(ArgumentParser=argparse.ArgumentParser):
     parser = ArgumentParser(
@@ -18,44 +15,44 @@ def get_argparser(ArgumentParser=argparse.ArgumentParser):
                      'Requires FontForge with Python bindings. '
                      'Stores the patched font as a new, renamed font file by default.')
     )
-    parser.add_argument('target_fonts', help='font files to patch', metavar='font',
-                        nargs='*', type=argparse.FileType('rb'))
+    parser.add_argument('target_font_path', help='font file to patch', metavar='font',
+                        type=argparse.FileType('rb'))
+    parser.add_argument('new_font_name', help='name of the produced font', metavar='name',
+                        type=str)
     return parser
-    
-def out_path(name):
-    Path('artifact').mkdir(parents=True, exist_ok=True)
-    return 'artifact/{0}.ttf'.format(name)
 
-def patch_one_font(font):
-    font.encoding = 'ISO10646'
-    mod_name = 'Photofonte'
-    font.familyname += ' with '+mod_name
-    font.fullname += ' with '+mod_name
-    fontname, style = FONT_NAME_RE.match(font.fontname).groups()
-    font.fontname = fontname + 'with' + mod_name
-    if style is not None:
-        font.fontname += style
-    font.appendSFNTName(
-        'English (US)', 'Preferred Family', font.familyname)
-    font.appendSFNTName(
-        'English (US)', 'Compatible Full', font.fullname)
-    
-    # 0xE900 starts an area spanning until 0xF000 that as far as I can tell nothing
-    # popular uses. I checked the Apple glyph browser and Nerd Font.
-    # Uses an array because of python closure capture semantics
-    encoding_alloc = [0xE900]
+def add_temp_glyph(font, name, original="a"):
+    #first decompile the objects
+    numGlyphs = font["maxp"].numGlyphs
+    locations = font["loca"].locations
+    glyphs = font["glyf"].glyphs
+    glyphOrder = font["glyf"].glyphOrder
+    metrics = font["hmtx"].metrics
+
+    #then modify them
+    locations.append(locations[numGlyphs])
+    font["maxp"].numGlyphs = numGlyphs + 1
+    glyphs[name] = glyphs[original]
+    glyphOrder.append(name)
+    metrics[name] = metrics[original]
+
+def patch_font(target_font_path, new_font_name):
+    font = TTFont(target_font_path.name)
+    font['name'].setName(new_font_name,1,3,1,1033)
+
+    add_temp_glyph(font, "tmp0")
 
     feature = """
 languagesystem DFLT dflt;
 languagesystem latn dflt;
 
 feature calt {
-    sub b r by seven;
-    sub seven by uni1E07 uni1E5F;
+    sub a m by tmp0;
+    sub tmp0 by uni1E06 uni1E5F;
 
     sub B r by six;
     sub six by uni1E06 uni1E5F;
-
+#
 #    sub d r by  d_r;
 #    sub d_r by uni1E0F uni1E5F;
 #
@@ -72,28 +69,13 @@ feature calt {
     with open('mods.fea', 'w') as f:
         f.write(feature)
 
-    Path('tmp').mkdir(parents=True, exist_ok=True)
-    font.generate('tmp/tmp.ttf')
-    ft_font = TTFont('tmp/tmp.ttf')
-    addOpenTypeFeatures(ft_font, 'mods.fea', tables=['GSUB'])
-    # replacement to comply with SIL Open Font License
-    out_name = font.fullname.replace('Source ', 'Sauce ')
-    ft_font.save(out_path(out_name))
-    print("> Created '{}'".format(out_name))
-    return out_name
-
-def patch_fonts(target_files):
-    res = []
-    for target_file in target_files:
-        target_font = fontforge.open(target_file.name)
-        try:
-            res.append(patch_one_font(target_font))
-        finally:
-            target_font.close()
-    return res
+    addOpenTypeFeatures(font, 'mods.fea', tables=['GSUB'])
+    font.save(f"{new_font_name}.ttf")
+    font.close()
+    print("> Created '{}'".format(new_font_name))
 
 def main(argv):
     args = get_argparser().parse_args(argv)
-    return patch_fonts(args.target_fonts)
+    return patch_font(args.target_font_path, args.new_font_name)
 
 main(sys.argv[1:])
